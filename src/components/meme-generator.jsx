ce679752e000b1bcd850421e5280dd372132d637
JLerry05 <jlerry005@gmail.com>
2023-03-09 22:23:21 +0800

    03-09-23

--- src/components/meme-generator.jsx
+++ src/components/meme-generator.jsx
@@ -3,6 +3,16 @@ import { React, useState, useEffect } from 'react'
 
 export default function Meme() {
 
+    /**
+        useEffect takes a function as its parameter. If that function
+        returns something, it needs to be a cleanup function. Otherwise,
+        it should return nothing. If we make it an async function, it
+        automatically retuns a promise instead of a function or nothing.
+        Therefore, if you want to use async operations inside of useEffect,
+        you need to define the function separately inside of the callback
+        function, as seen below:
+    */
+
     const [meme, setMeme] = useState({
         topText: "",
         bottomText: "",
